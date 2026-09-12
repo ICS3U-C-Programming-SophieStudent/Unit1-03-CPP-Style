28ea370d75ca4697d9c4a23643e24db6d23a0cb8
Sophie Student <sophie.student16@gmail.com>
2026-09-12 18:12:40 +0000

    First commit of helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2026 Sophie Student All rights reserved.
+// .
+// Created by: Sophie Student
+// Date: Sep. 12, 2026
+// This program displays a greeting with proper style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Sophie!" << std::endl;
+    std::cout << "Programming is SO much FUN!\n";
+    std::cout << "I cannot wait to learn more:)" << std::endl;
+}
